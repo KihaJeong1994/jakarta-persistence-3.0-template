e96dc1113c22bf2290c4d50bf61954eb942ef4fb
KihaJeong1994 <jkha7371@gmail.com>
2023-05-22 09:22:22 +0900

    test class & method name changed

--- src/test/java/com/example/jpatemplate/EntityLifeCycleTest.java
+++ src/test/java/com/example/jpatemplate/EntityLifeCycleTest.java
@@ -1,7 +1,6 @@
 package com.example.jpatemplate;
 
 import com.example.jpatemplate.address.Address;
-import com.example.jpatemplate.user.Customer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
@@ -11,25 +10,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
-public class EntityManagerTest {
+public class EntityLifeCycleTest {
 
     @PersistenceContext
     EntityManager em;
 
     @Test
-    void persist_test(){
-        var customer1 = new Customer();
-        customer1.setId(1L);
-        customer1.setCustomerId("abcd");
-        em.persist(customer1);
+    void managed_entity_sync_to_db_test(){
+        var address1 = new Address();
+        address1.setId(1L);
+        address1.setCountry("USA");
+        address1.setDetails("California");
+        em.persist(address1);
         // persistence context is flushed to database -> insert query
         em.flush();
         // if I detach the entity manager from persistence context(em.clear()) -> em.find() select data from database. if not, search from persistence context, so no select query execute
         em.clear();
         // detach the entity instance from persistence context
         //em.detach(customer1);
-        var customerFromContext = em.find(Customer.class, 1L);
-        assertEquals(customer1.getCustomerId(),customerFromContext.getCustomerId());
+        var addressFromContext = em.find(Address.class, 1L);
+        assertEquals(address1.getCountry(),addressFromContext.getCountry());
+        assertEquals(address1.getDetails(),addressFromContext.getDetails());
     }
 
 }
